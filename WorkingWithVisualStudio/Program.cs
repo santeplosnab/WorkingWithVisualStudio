3ba5e48c76f07443f00b6bbc0c76589442088840
eshxe <eshxe@mail.ru>
2017-08-24 13:56:50 +0400

    create environment

--- WorkingWithVisualStudio/Program.cs
+++ WorkingWithVisualStudio/Program.cs
@@ -16,7 +16,6 @@ namespace WorkingWithVisualStudio
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
